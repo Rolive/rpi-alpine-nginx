7acb8b5ac239af8a89f36546452cd2a757db133e
Rolive <drakerinproduction@gmail.com>
2016-03-29 17:16:14 +0200

    improved modularity

--- root/start-nginx.sh
+++ root/start-nginx.sh
@@ -7,6 +7,7 @@ OVERRIDE="/tmp/conf_override"
 
 CONFIG="nginx.conf"
 VHOSTS="conf.d"
+FILES="/var/www"
 
 cd "$NGINX"
 
@@ -22,6 +23,12 @@ fi
 if [[ -f "$OVERRIDE/$CONFIG" ]]; then
   rm -f "$CONFIG"
   ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
+fi
+
+#Symlink websites files
+if [[ -d "$OVERRIDE/$FILES" ]]; then
+  rm -rf "$FILES"
+  ln -s "$OVERRIDE/$FILES" "$FILES"
 fi
 
 exec nginx
